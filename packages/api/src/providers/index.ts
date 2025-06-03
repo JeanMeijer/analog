d716cb2539608c969fa100195e3075b842e49151
Sergio Vazquez <sergiovazag@gmail.com>
2025-06-03 11:09:36 -0600

    feat: tasks
    feat: endpoints
    
    feat: rename taskList to category
    
    feat: add missing actions
    
    feat: add task component

--- packages/api/src/providers/index.ts
+++ packages/api/src/providers/index.ts
@@ -1,15 +1,21 @@
 import { account } from "@repo/db/schema";
 
-import { GoogleCalendarProvider } from "./google-calendar";
-import type { CalendarProvider } from "./interfaces";
-import { MicrosoftCalendarProvider } from "./microsoft-calendar";
+import { GoogleCalendarProvider } from "./calendars/google-calendar";
+import { MicrosoftCalendarProvider } from "./calendars/microsoft-calendar";
+import type { CalendarProvider, TaskProvider } from "./interfaces";
+import { GoogleTasksProvider } from "./tasks/google-tasks";
+import { google } from "better-auth/social-providers";
 
 const supportedProviders = {
   google: GoogleCalendarProvider,
   microsoft: MicrosoftCalendarProvider,
 } as const;
 
-export function accountToProvider(
+const supportedTaskProviders = {
+  google: GoogleTasksProvider,
+} as const;
+
+export function accountToCalendarProvider(
   activeAccount: typeof account.$inferSelect,
 ): CalendarProvider {
   if (!activeAccount.accessToken || !activeAccount.refreshToken) {
@@ -17,6 +23,23 @@ export function accountToProvider(
   }
 
   const Provider = supportedProviders[activeAccount.providerId];
+
+  if (!Provider) {
+    throw new Error("Provider not supported");
+  }
+
+  return new Provider({ accessToken: activeAccount.accessToken });
+}
+
+
+export function accountToTasksProvider(
+  activeAccount: typeof account.$inferSelect,
+): TaskProvider {
+  if (!activeAccount.accessToken || !activeAccount.refreshToken) {
+    throw new Error("Invalid account");
+  }
+
+  const Provider = supportedTaskProviders["google"];
 
   if (!Provider) {
     throw new Error("Provider not supported");
